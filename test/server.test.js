f06c27436f7471ad02e717718d3b3f294e4b107d
Kyle <kgbostian@gmail.com>
2021-10-19 23:24:00 -0500

    Nightly push.

--- test/server.test.js
+++ test/server.test.js
@@ -1,6 +1,8 @@
 const app = require("../server")
 const supertest = require("supertest");
 const { query } = require("express");
+const { graphql } = require("graphql");
+const Schema = require("../database/schema/schema")
 
 
 // This passes because 1 === 1
@@ -46,34 +48,88 @@ const seed = () => {
 //   } }`
 // };
 
-
+// function getUsers() {
+//   try {
+//     graphql(Schema, '{firstName}', '/graphql/user').then((response) => { console.log(response);});
+//   } catch(error) {
+//     console.log(error);
+//   }
+    // try {
+  //   supertest(app)
+  //     .post("/graphql")
+  //     .send({query :`{ users{firstName} }`})
+  //     .then((response) => {      
+  //       // Check type and length
+  //       console.log(response.text); 
+  //       expect(response.text.includes("name"));
+  //     })
+  // } catch (error) {
+  //   console.log(error.text);
+  // }
+// }
 
 test(" GET /api/posts", async () => {
   try {
     await supertest(app)
       .post("/graphql")
       .send({query :`{ users{firstName} }`})
-      .expect(200)
       .then((response) => {      
         // Check type and length
         console.log(response.text); 
-        expect(response.text.includes("Don"));
+        expect(response.text.includes("name"));
       })
   } catch (error) {
-    console.log("ERROR: ERROR");
+    console.log(error.text);
   }
-  // try {
-  //   await supertest(app)
-  //     .post("/graphql")
-  //     .send({query :`mutation { addUser( firstName:"Travis") { firstName } }` })
-  //     // .expect(200)
-  //     .then((response) => {      
-  //       // Check type and length
-  //       console.log(response.text); 
-  //     })
-  // } catch (error) {
-  //   console.log("ERROR: ERROR");
-  // }
+  try {
+    await supertest(app)
+      .post("/graphql")
+      .send({query :`mutation { addUser( firstName:"Travis") { firstName } }` })
+      // .expect(200)
+      .then((response) => {      
+        // Check type and length
+        console.log(response.text); 
+      })
+  } catch (error) {
+    console.log(error);
+  }
+  try {
+    supertest(app)
+      .post("/graphql")
+      .send({query :`{ users{firstName} }`})
+      .then((response) => {      
+        // Check type and length
+        console.log(response.text); 
+        expect(response.text.includes("name"));
+      })
+  } catch (error) {
+    console.log(error.text);
+  }
+  try {
+    await supertest(app)
+      .post("/graphql")
+      .send({query :`mutation { removeUser( firstName:"Travis") { firstName } }` })
+      // .expect(200)
+      .then((response) => {      
+        // Check type and length
+        console.log(response.text); 
+      })
+  } catch (error) {
+    console.log(error);
+  }
+  try {
+    await supertest(app)
+      .post("/graphql")
+      .send({query :`{ users{firstName} }`})
+      .expect(400)
+      .then((response) => {      
+        // Check type and length
+        console.log(response.text); 
+        expect(false);
+      })
+  } catch (error) {
+    console.log(error.text);
+  }
 });
 // test(" Add Song", async () => {
 //   try {
